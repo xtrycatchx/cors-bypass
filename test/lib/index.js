0b6747157b2b980089b70a47375394bd54330ac3
Paul Sydney <xtrycatchx@gmail.com>
2018-04-24 21:35:17 +0800

    added tests for proxy started and stopped

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const CorsBypass = require('../../lib')
+const net = require('net');
 
 describe('Creating new instance', () => {
     it('should fail because no target', () => {
@@ -29,5 +30,29 @@ describe('Creating new instance', () => {
         const cors = new CorsBypass(target, port)
         expect(cors.info().target).to.equal(target)
         expect(cors.info().port).to.equal(port)
+    })
+
+    it('should be able to start', () => {
+        const target = 'http://google.com'
+        const port = 2000
+        const cors = new CorsBypass(target, port)
+        expect(cors.info().target).to.equal(target)
+        expect(cors.info().port).to.equal(port)
+
+        cors.start()
+
+
+        const client = new net.Socket();
+        client.connect(port, '127.0.0.1', () => {
+            expect(cors.isStarted()).to.be.true
+            client.destroy()
+        });
+
+        client.on('close', () => {
+            cors.stop();
+            expect(cors.isStarted()).to.be.false
+        });
+
+
     })
 })
